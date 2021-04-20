e2457013ea1fdb9faea1ed58335db49d7921649a
Cruiser Baxter <cruiser.j.baxter@gmail.com>
2021-04-20 09:29:21 -0700

    added composer.json and index.php

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+// controller for the diner project
+
+// turn on error reporting
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+require_once ('vendor/autoload.php');
+
+// instantiate Fat-Free
+$f3 = Base::isntance();
