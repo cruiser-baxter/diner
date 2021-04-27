6f20c6c9bab24bc84097277c2eae51b191911472
Cruiser Baxter <cruiser.j.baxter@gmail.com>
2021-04-27 09:10:11 -0700

    added .htaccess config file

--- index.php
+++ index.php
@@ -17,5 +17,13 @@ $f3->route('GET /', function (){
     echo $view->render('views/home.html');
 });
 
+//
+$f3->route('GET /breakfast', function (){
+    // instantiate a view object
+    //$view = new Template();
+    //echo $view->render('views/home.html');
+    echo "<h2>What's for breakfast?</h2>";
+});
+
 // run Fat-Free
 $f3->run();
